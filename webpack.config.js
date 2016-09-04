5803c53de92b5b8b626091aa8b0a3dc10d1d5a25
Matthew Hager <matthew@poeticsystems.com>
2016-09-03 21:33:02 -0500

    Setup base build tools.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/stanza.js',
+  output: {
+    path: './bin',
+    filename: 'stanza.bundle.js',
+    libraryTarget: 'commonjs2',
+    library: 'stanza',
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint',
+      },
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
