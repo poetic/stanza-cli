cc80ac7a530640433e8b0ff7d27082492348ca48
Matthew Hager <matthew@poeticsystems.com>
2016-09-11 14:08:41 -0500

    Switch from JsDoc to ESDoc.

--- src/stanza.js
+++ src/stanza.js
@@ -1,17 +1,13 @@
 import commander from 'commander';
-import pkgConf from 'pkg-conf';
-
 import packageJson from '../package.json';
 import registerExtensions from './imports/register-extensions';
 
 /**
- * Class representing Stanza
+ * Stanza class.
  */
 export default class Stanza {
   /**
-   *
-   *
-   * @returns {undefined}
+   * Initializes commander and registers extensions.
    */
   constructor() {
     commander
@@ -23,34 +19,9 @@ export default class Stanza {
 
   /**
    * Runs the command line interface.
-   *
    * @returns {undefined}
    */
   cli() {
     commander.parse(process.argv);
-  }
-
-  /**
-   * Determines the project's root path by finding the nearest package.json and
-   * checking it for the "stanza-project" keyword.
-   *
-   * @returns {string|false} project's root path or false if not in a project.
-   */
-  get projectRoot() {
-    // memoize
-    if(typeof this._projectRoot !== 'undefined') {
-      return this._projectRoot;
-    }
-
-    const packageConfig = pkgConf.sync('keywords');
-    const isStanzaProject = Object.keys(packageConfig)
-      .map(key => packageConfig[key])
-      .includes('stanza-project');
-
-    this._projectRoot = isStanzaProject
-      ? path.dirname(pkgConf.filepath(packageConfig))
-      : false;
-
-    return this._projectRoot;
   }
 }
