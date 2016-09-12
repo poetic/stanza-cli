9b1237b9f2f6adfec5f81ded37dc95f03055f7fd
Kristy Miller <miller.kristy06@gmail.com>
2016-09-12 15:09:16 -0500

    Logger is accessible by extensions from the Stanza object

--- src/stanza.js
+++ src/stanza.js
@@ -1,6 +1,7 @@
 import commander from 'commander';
 import packageJson from '../package.json';
 import registerExtensions from './imports/register-extensions';
+import logger from './imports/logger';
 
 /**
  * Stanza class.
@@ -10,6 +11,8 @@ export default class Stanza {
    * Initializes commander and registers extensions.
    */
   constructor() {
+    this.logger = logger;
+
     commander
       .version(packageJson.version)
       .description(packageJson.description);
