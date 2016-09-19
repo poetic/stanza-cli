ab6750388e62714de4970c23f38febe6ce4f67a0
Kristy Miller <miller.kristy06@gmail.com>
2016-09-19 11:06:12 -0500

    Logger docs and todos

--- src/imports/logger.js
+++ src/imports/logger.js
@@ -1,10 +1,20 @@
 import winston from 'winston';
 
+/**
+ * @type {Object} Instantiated Winston Logger
+ */
 const logger = new winston.Logger;
 
+
+// TODO: verbose levels for logging
+// default (error, warn)
+// pass flags for:
+// debug
+// verbose
+// set the param on the logging config to either debug or verbose
+// check defaults to make sure we arn't setting the same values
 logger.add(winston.transports.Console, {
   colorize: true,
-  level: 'debug',
   prettyPrint: true,
   silent: false,
   timestamp: true,
