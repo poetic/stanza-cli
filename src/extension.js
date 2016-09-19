28ecc3a37b67c5526cba022d8ad3f6b53f33a636
Kristy Miller <miller.kristy06@gmail.com>
2016-09-19 10:19:20 -0500

    Update docs for Extension class

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,5 @@
 import glob from 'glob';
-import path from 'path';
 import resolve from 'resolve';
-import yeoman from 'yeoman-environment';
 
 /**
  * Class representing an Extension
@@ -9,8 +7,7 @@ import yeoman from 'yeoman-environment';
 class Extension {
   /**
    * When an extension is instantiated, pass in the object in which to register
-   * with and the module keyword; Run discoverChildren to register commands and
-   * generators with Stanza.
+   * with and the module keyword
    *
    * @param {string} name Extension Name
    * @param {Object} registerWithObject Object in which to register commands and
@@ -24,62 +21,32 @@ class Extension {
     this._extensionPath = extensionPath;
     this._registerWithObject = registerWithObject;
     this._keyword = keyword;
-    this._yeomanEnv = yeoman.createEnv();
 
     this._commands = [];
-    this._generators = [];
 
     this.discoverCommands();
-    this.discoverGenerators();
   }
 
   /**
-   * Find extenstion commands and register them with Stanza
+   * Find extension commands and instantiate the class, pass command to the
+   * Class; Commands are responsible for registering with Stanza
    *
    * @name discoverCommands
    * @returns {undefined}
    */
   discoverCommands() {
-    const commands = glob.sync('commands/**.js', { cwd: this._extensionPath });
+    const commands = glob.sync('commands/*.js', { cwd: this._extensionPath });
 
-    commands.forEach(commandFileName => {
+    this._commands = commands.map(commandFileName => {
       const commandPath = resolve.sync(`${this._extensionPath}/${commandFileName}`);
-      const command = require(commandPath);
+      const Command = require(commandPath).default;
+      const command = new Command(
+        this._name,
+        this._extensionPath,
+        this._registerWithObject.commander
+      );
 
-      this._commands.push(command);
-
-      const additionalParams = {
-        name: this._name,
-      };
-
-      if (command.pattern.includes('generate')) {
-        additionalParams.yeomanEnv = this._yeomanEnv;
-      }
-
-      this._registerWithObject.commander
-        .command(command.pattern)
-        .description(command.description)
-        .action((arg, options) => command.action(arg, options, additionalParams));
-    });
-  }
-
- /**
-   * Find extenstion generators and register them with Stanza
-   *
-   * @name discoverGenerators
-   * @returns {undefined}
-   */
-  discoverGenerators() {
-    const generators = glob.sync('generators/*/index.js', { cwd: this._extensionPath });
-
-    generators.forEach(generator => {
-      const generatorPath = resolve.sync(`${this._extensionPath}/${generator}`);
-      const directory = path.dirname(generatorPath).split('/').pop();
-      const namespace = `${this._name}:${directory}`;
-
-      this._generators.push({ generatorPath, namespace });
-
-      this._yeomanEnv.register(generatorPath, namespace);
+      return command;
     });
   }
 }
