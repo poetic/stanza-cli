6bfd68c3122c5054d252e88bb36e47866426c2e2
Kristy Miller <miller.kristy06@gmail.com>
2016-09-29 15:24:43 -0500

    Complete docs for Command class

--- src/command.js
+++ src/command.js
@@ -1,30 +1,69 @@
+/**
+ * Class representing a Command
+* */
 export default class Command {
+  /**
+   * @param {string} extensionName The name of the extension registering commands
+   * @param {string} extensionPath Absolute path to the extension
+   * @param {Object} command Instance of Commandjs
+   */
   constructor(extensionName, extensionPath, commander) {
     this._commander = commander;
     this._extensionName = extensionName;
     this._extensionPath = extensionPath;
   }
 
+  /**
+   * Set pattern for the command
+   *
+   * @param {string} command Pattern to register with Commanderjs
+   * @return {undefined}
+   */
   setCommand(command) {
     this.command = command;
   }
 
+  /**
+   * Get pattern for the command
+   *
+   * @return {string} Command pattern
+   */
   getCommand() {
     return this.command;
   }
 
+  /**
+   * Set command description
+   *
+   * @param {string} description Description of what this command does
+   * @return {undefined}
+   */
   setDescription(description) {
     this.description = description;
   }
 
+  /**
+   * Get command description
+   *
+   * @return {string} Command description
+   */
   getDescription() {
     return this.description;
   }
 
+  /**
+   * Set command action
+   *
+   * @param {function} action What should happen when the command is called
+   * @return {undefined}
+   */
   setAction(action) {
     this.action = action;
   }
 
+  /**
+   * Register commands with Commandjs
+   */
   registerCommand() {
     this._commander
       .command(this.command)
